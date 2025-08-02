81a982291416a7c9b5c6a537bd50d7a6d96041c4
Irving Rangel Skasurck <skasurck@gmail.com>
2025-08-02 10:17:31 -0600

    Redirigir checkout desde carrito

--- frontend/src/views/CarritoView.vue
+++ frontend/src/views/CarritoView.vue
@@ -59,10 +59,12 @@
 
 <script setup>
 import { onMounted, computed } from 'vue'
+import { useRouter } from 'vue-router'
 import { useCarritoStore } from '../stores/carrito'
 import IconTrash from '../components/icons/IconTrash.vue'
 
 defineOptions({ name: 'CarritoView' })
+const router = useRouter()
 const carrito = useCarritoStore()
 
 onMounted(() => {
@@ -97,6 +99,6 @@ const cambiar = (item, cantidad) => {
   }
 }
 const finalizarCompra = () => {
-  alert('Procesar compra...')
+  router.push('/checkout')
 }
 </script>
