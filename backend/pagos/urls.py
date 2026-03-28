d6a7ccec92f16bbb52a399fc684a418717edb7a4
Irving Rangel Skasurck <skasurck@gmail.com>
2026-03-27 20:15:26 -0600

    update url
      path('mercadopago/webhook/', MercadoPagoWebhookView.as_view(), name='mercadopago-webhook'),

--- backend/pagos/urls.py
+++ backend/pagos/urls.py
@@ -3,5 +3,5 @@ from .views import MercadoPagoPreferenceView, MercadoPagoWebhookView
 
 urlpatterns = [
     path('mercadopago/preferencias/', MercadoPagoPreferenceView.as_view(), name='mercadopago-preference'),
-    path('webhooks/mercadopago/', MercadoPagoWebhookView.as_view(), name='mercadopago-webhook'),
+    path('mercadopago/webhook/', MercadoPagoWebhookView.as_view(), name='mercadopago-webhook'),
 ]
