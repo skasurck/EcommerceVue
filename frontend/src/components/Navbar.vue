50736469f139fc68ff583527d36efaaaffbc19b3
Irving Rangel Skasurck <skasurck@gmail.com>
2026-03-29 17:59:17 -0600

    cambios logos

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -12,8 +12,8 @@
 
           <!-- Logo -->
           <RouterLink to="/" class="flex items-center shrink-0">
-            <img src="/logo-mktska.png" alt="Mktska Digital" width="120" height="36" class="h-9 w-auto object-contain dark:hidden" />
-            <img src="/logo-mktska-white.png" alt="Mktska Digital" width="120" height="36" class="h-9 w-auto object-contain hidden dark:block" />
+            <img src="/logo-mktska.webp" alt="Mktska Digital" width="120" height="36" class="h-9 w-auto object-contain dark:hidden" />
+            <img src="/logo-mktska-white.webp" alt="Mktska Digital" width="120" height="36" class="h-9 w-auto object-contain hidden dark:block" />
           </RouterLink>
 
           <!-- Ubicación -->
@@ -178,8 +178,8 @@
           </button>
 
           <RouterLink to="/" class="flex items-center shrink-0">
-            <img src="/logo-mktska.png" alt="Mktska Digital" width="120" height="32" class="h-8 w-auto object-contain dark:hidden" />
-            <img src="/logo-mktska-white.png" alt="Mktska Digital" width="120" height="32" class="h-8 w-auto object-contain hidden dark:block" />
+            <img src="/logo-mktska.webp" alt="Mktska Digital" width="120" height="32" class="h-8 w-auto object-contain dark:hidden" />
+            <img src="/logo-mktska-white.webp" alt="Mktska Digital" width="120" height="32" class="h-8 w-auto object-contain hidden dark:block" />
           </RouterLink>
 
           <!-- User greeting -->
