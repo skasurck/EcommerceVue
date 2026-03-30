ada5c2728073e4ddf02a84d7e0cfe764d1ac5bbe
Irving Rangel Skasurck <skasurck@gmail.com>
2026-03-29 22:49:47 -0600

    updata pear 2fa

--- backend/tienda/urls.py
+++ backend/tienda/urls.py
@@ -25,10 +25,11 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 from productos.views import editar_producto
+from two_factor.urls import urlpatterns as tf_urls
 
 urlpatterns = [
     # Login 2FA para el panel de Django Admin
-    path('mktska-panel-x7k2/', include(('two_factor.urls', 'two_factor'))),
+    path('mktska-panel-x7k2/', include((tf_urls, 'two_factor'))),
     path('mktska-panel-x7k2/admin/', admin.site.urls),
     path('productos/editar/<int:pk>/', editar_producto, name='editar_producto'),
     path('api/', include('productos.urls')),
